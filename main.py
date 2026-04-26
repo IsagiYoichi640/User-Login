2e11dcd5fdad1af127d0895f4ad79ef585ef651c
IsagiYoichi640 <arvinshrestha11@gmail.com>
2026-04-26 21:44:59 +0545

    cw

--- main.py
+++ main.py
@@ -24,12 +24,100 @@ lm.login_view='login'
 serializer=URLSafeTimedSerializer(app.config['SECRET_KEY'])
 #Database for users
 class User(db.Model,UserMixin):
-    id=db.Column(db.Interger,primary_key=True)
+    id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(100))
-    email=db.Column(db.Strung(100),unique=True)
+    email=db.Column(db.String(100),unique=True)
     password=db.Column(db.String(200))
 #load users
 @lm.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 #routes
+@app.route('/')
+def home():
+    return redirect(url_for('login'))
+#register
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method=='POST':
+        username=request.form['username']
+        email=request.form['email']
+        password=request.form['password']
+        confirm_password=request.form['confirm_password']
+        if password!=confirm_password:
+            flash('Passwords do not match','danger')
+            return redirect(url_for('register'))
+        hashed=generate_password_hash(password)
+        user=User(username=username,email=email,password=hashed)
+        #save user details
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created successfully",'success')
+        return redirect(url_for('login'))
+    return render_template('register.html')
+#login
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        user=User.query.filter_by(username=request.form['username']).first()
+        if user and check_password_hash(user.password,request.form['password']):
+            login_user(user)
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Invalid credentials', 'danger')
+    return render_template('login.html')
+#dashboard
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+#logout
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+#forgot password
+@app.route('/forgot',methods=['GET','POST'])
+def forgot():
+    if request.method=='POST':
+        email=request.form['email']
+        user=User.query.filter_by(email=email).first()
+        if user:
+            token=serializer.dumps(email,salt='reset-password')
+            link=url_for('reset',token=token,_external=True)
+            msg=Message(
+                'Password Rest Link',
+                sender='arvinshrestha11@gmail.com',
+                recipients=[email]
+            )
+            msg.body=f'Click on the link below to reset your password:{link}'
+            mail.send(msg)
+            flash('Reset Link sent to your email','success')
+        else:
+            flash('Email not found','danger')
+    return render_template('forgot.html')
+#reset password
+@app.route('/reset/<token>',methods=['GET','POST'])
+def reset(token):
+    try:
+        email=serializer.loads(token,salt='reset-password',max_age=300)
+    except:
+        flash('Invalid link','danger')
+        return redirect(url_for('forgot'))
+    if request.method=='POST':
+        password=request.form['password']
+        confirm=request.form['confirm']
+        if password!=confirm:
+            flash('password does not match','danger')
+            return redirect(request.url)#reload the page
+        user=User.query.filter_by(email=email).first()
+        user.password=generate_password_hash(password)
+        db.session.commit()
+        flash('Password changed successfully','success')
+        return redirect(url_for('login'))
+    return render_template('reset.html')
+if __name__=='__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
